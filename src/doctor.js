a5fa7316a61cf2349844e9c7e81f99269cbff25b
julius bade <julbade21@gmail.com>
2018-10-19 10:43:06 -0700

    add functionalities and form input

---
+++ src/doctor.js
@@ -0,0 +1,17 @@
+export class Search {
+  searchName(name) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&limit=10&user_key=$(process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Search } from './doctor.js';
 
 $(document).ready(function() {
+  $('#doctorForm').sumbit(function() {
+    let name = $('#doctorName').val();
+    $('#doctorName').val("");
+
+
+    let doctorSearch = new Search();  // create instance of WeatherService class
+    let promise = doctorSearch.searchName(name);  // call the instance method and pass in user input
+
+    promise.then(function(response) {
+      body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 
 });
