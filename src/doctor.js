bf894a5a2d93e6e7bcae85694b658f979bd4676a
julius bade <julbade21@gmail.com>
2018-10-20 09:23:20 -0700

    add error return message

--- src/doctor.js
+++ src/doctor.js
@@ -4,7 +4,7 @@ export class Search {
   searchDoctor(name, specialty) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&name=${name}&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&name=${name}&specialty_name=${specialty}&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
