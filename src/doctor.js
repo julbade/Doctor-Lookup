18a45b6a1aa3a8ed1f6806de8426b04af331755f
julius bade <julbade21@gmail.com>
2018-10-23 21:46:03 -0700

    Working functions and methods

--- src/doctor.js
+++ src/doctor.js
@@ -4,14 +4,14 @@ export class Search {
   searchDoctor(name, specialty) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&name=${name}&specialty_name=${specialty}&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${specialty}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
--- src/main.js
+++ src/main.js
@@ -2,22 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Search, Doctor } from './doctor.js';
+import { Doctor, Search } from './doctor.js';
 /* eslint-disable no-unused-vars */
 $(document).ready(function() {
-  $('#doctorForm').submit(function(event) {
+  $('form#doctor').submit(function(event) {
     event.preventDefault();
     let name = $('#doctorName').val();
-    let specialty = $("#specialty").val();
+    let specialty = $('#specialty').val();
     $('#doctorName').val("");
     $('#specialty').val("");
-
 
     let doctorSearch = new Search();
     let promise = doctorSearch.searchDoctor(name, specialty);
 
     promise.then(function(response) {
     let body = JSON.parse(response);
+    if (body.data.length === 0) {
+      $("#errors").text("There were no doctors found matching your search criteria.");
+    }
     for (let i = 0; i < body.data.length; i++) {
            let firstName = body.data[i].profile.first_name;
            let lastName = body.data[i].profile.last_name;
@@ -25,17 +27,20 @@ $(document).ready(function() {
              ${body.data[i].practices[0].visit_address.street2}
              ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}`;
            let phone = body.data[i].practices[0].phones[0].number;
-           let language = body.data[i].practices[0].languages;
+           let language = body.data[i].practices[0].languages[0].name;
            let doctor = new Doctor(firstName, lastName, address, phone, language);
-           console.log(doctor)
-           $("#results").append(doctor);
+           
+           $("#results").append(`<h2> ${doctor.firstName}   ${doctor.lastName} </h2>` +
+           ` <p>Address:  ${doctor.address} </p>` +
+           `<p>Phone: ${doctor.phone} </p>` + 
+           `<p>Language: ${doctor.language} </p>` +`<hr>` );
          }
    },
    function(error) {
       $('#errors').text(`There was an error processing your request. Please try again. ${error}`);
 
 
-
+    });
   });
- });
 });
+
