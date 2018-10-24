520195295d48e7106a2781b1a349ff653092f1a4
julius bade <julbade21@gmail.com>
2018-10-23 22:40:40 -0700

    changed limit for api display

--- src/doctor.js
+++ src/doctor.js
@@ -4,7 +4,7 @@ export class Search {
   searchDoctor(name, specialty) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${specialty}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${specialty}&location=wa-seattle&skip=0&limit=15&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
